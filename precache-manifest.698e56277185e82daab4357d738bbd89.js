731dc4af6ef7ad91ab0a940f5b3e914587d37f23
Jason Britto <35361130+john9088@users.noreply.github.com>
2020-08-01 01:29:47 +0530

    Updates

---
+++ precache-manifest.698e56277185e82daab4357d738bbd89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8456efa72ee57d3e2d39db0add8776b8",
+    "url": "/React-Recipe_app/index.html"
+  },
+  {
+    "revision": "4a9893f567b205ed026b",
+    "url": "/React-Recipe_app/static/css/main.930b56e4.chunk.css"
+  },
+  {
+    "revision": "05fb40bf0dbbbef7377e",
+    "url": "/React-Recipe_app/static/js/2.35f3cf29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Recipe_app/static/js/2.35f3cf29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a9893f567b205ed026b",
+    "url": "/React-Recipe_app/static/js/main.b19f3530.chunk.js"
+  },
+  {
+    "revision": "dac81e7d3eefbb415dc8",
+    "url": "/React-Recipe_app/static/js/runtime-main.56b311f6.js"
+  }
+]);
